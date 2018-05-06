3b860713d8123f2f7106671e4c4604942fec568b
Sean Kennedy <sean@kndy.net>
2018-05-05 22:10:20 -0600

    add timezone to Formatters\Date

--- src/Formatters/Date.php
+++ src/Formatters/Date.php
@@ -4,13 +4,15 @@ namespace SeanKndy\CSV\Formatters;
 class Date implements Formatter
 {
     protected $format;
+    protected $tz;
 
-    public function __construct($format) {
+    public function __construct($format, $tz = 'America/Denver') {
         $this->format = $format;
+        $this->tz = $tz;
     }
 
     public function format($data) {
-        $date = new \DateTime(trim($data));
+        $date = new \DateTime(trim($data), new \DateTimeZone($this->tz));
         return $date->format($this->format);
     }
 }
