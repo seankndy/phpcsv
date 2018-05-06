1cbde74fd60f507e75a85edef49f78a5f074bc48
Sean Kennedy <sean@kndy.net>
2018-05-06 17:38:36 -0600

    added Numeric Formatter

--- src/Formatters/Alphanumeric.php
+++ src/Formatters/Alphanumeric.php
@@ -4,6 +4,6 @@ namespace SeanKndy\CSV\Formatters;
 class Alphanumeric implements Formatter
 {
     public function format($data) {
-        return preg_replace('/[^\W]/', '', $data);
+        return preg_replace('/[\W]/', '', $data);
     }
 }
