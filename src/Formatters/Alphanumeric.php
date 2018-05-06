5b8998fdae54834397e98f28e357786cdfe1bf54
Sean Kennedy <sean@kndy.net>
2018-05-06 17:30:02 -0600

    added README and Alphanumeric Formatter

---
+++ src/Formatters/Alphanumeric.php
@@ -0,0 +1,9 @@
+<?php
+namespace SeanKndy\CSV\Formatters;
+
+class Alphanumeric implements Formatter
+{
+    public function format($data) {
+        return preg_replace('/[^\W]/', '', $data);
+    }
+}
